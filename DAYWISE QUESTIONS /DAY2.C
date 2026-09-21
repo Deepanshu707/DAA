c9857e9cec2b6dbd72eab5dedce60f37f7fbd2ab
Deepanshu707 <deepa9shu25200310@gmail.com>
2026-09-21 23:28:15 +0530

    Implement password prompt with while loop

--- DAYWISE QUESTIONS /DAY2.C
+++ DAYWISE QUESTIONS /DAY2.C
@@ -1,1 +1,27 @@
+//Write a program using a while loop that repeatedly asks the user to enter the password and stops only when the correct password is entered. 
+//Finally, display "Login successful!".
 
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char password[20];
+
+    while (1)
+    {
+        printf("Enter password: ");
+        scanf("%19s", password);
+
+        if (strcmp(password, "abc123") == 0)
+        {
+            break;
+        }
+
+        printf("Incorrect password. Try again.\n");
+    }
+
+    printf("Login successful!");
+
+    return 0;
+}
