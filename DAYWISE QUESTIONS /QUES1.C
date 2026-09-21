ff4026128d7e955129961540f1660799ce6a4b1a
Deepanshu707 <deepa9shu25200310@gmail.com>
2026-09-21 23:27:07 +0530

    Add C program to find missing digit in number

--- DAYWISE QUESTIONS /QUES1.C
+++ DAYWISE QUESTIONS /QUES1.C
@@ -1,1 +1,26 @@
+// Write a C program that accepts a positive integer containing digits from 0 to 9. 
+// Exactly one digit is missing, while the remaining nine digits appear exactly once. 
+// Input : 854102967 
+// Output: 3
 
+#include <stdio.h>
+
+int main()
+{
+    int n, digit;
+    int sum = 45;
+
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    while (n > 0)
+    {
+        digit = n % 10;
+        sum = sum - digit;
+        n = n / 10;
+    }
+
+    printf("Missing digit = %d", sum);
+
+    return 0;
+}
